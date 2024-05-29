1fe4c64a9df6aaf9a46bf285bb584cebdf3a1cb3
MohamedH1000 <mohammedhisham115@gmail.com>
2024-05-30 00:25:27 +0300

    portfolio

--- data/index.ts
+++ data/index.ts
@@ -1,5 +1,4 @@
 export const navItems = [
-  { name: "Portfolio", link: "/" },
   { name: "About", link: "#about" },
   { name: "Projects", link: "#projects" },
   { name: "Testimonials", link: "#testimonials" },
